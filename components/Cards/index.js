42b7e80f05a1306456818858c4300e2fd1fc04a9
Paul M Edwards <pauledwards@gmail.com>
2019-09-13 20:44:39 -0700

    MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,7 +1,7 @@
 // STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
+// Study the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
@@ -17,3 +17,87 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cards = document.querySelector('.cards-container');
+
+const articleData = fetchData('https://lambda-times-backend.herokuapp.com/articles');
+console.log("articleData:");
+console.log(articleData);
+
+appendArticles(cards, articleData);
+
+async function fetchData(remoteDataSourceUri) {
+  return await axios.get(remoteDataSourceUri);
+}
+
+function appendArticles(target, data) {
+  // console.log(`appendArticles Data:`);
+  // console.log(data);
+
+  data.then(r => {
+    const topics = r.data.articles;
+    console.log(`Status: `+r.status);
+    console.log('topics data:');
+    console.log(topics);
+
+    const articleTopicNames = ['bootstrap', 'javascript', 'jquery', 'node', 'technology'];
+    const articleTopics = [r.data.articles.bootstrap, r.data.articles.javascript, r.data.articles.jquery, r.data.articles.node, r.data.articles.technology];
+    console.log(articleTopics);
+    
+    const topicCount = articleTopics.length;
+    console.log('topicCount: '+topicCount);
+
+    for (let i = 0; i <= topicCount-1; i++) {
+    // parseTopics().forEach(t => {
+      const currentTopicName = articleTopicNames[i];
+      const currentTopic = articleTopics[i];
+
+      // console.log(i);
+      console.log('currentTopicName:');
+      console.log(currentTopicName);
+      console.log('currentTopic data:');
+      console.log(currentTopic);
+
+      const articleCount = currentTopic.length;
+      console.log('articleCount: '+articleCount);
+      
+      for (let j = 0; j <= articleCount-1; j++) {
+        target.appendChild(Article(currentTopic[j]));
+      }
+    // });
+    }
+  })
+  .catch(e => {
+    console.log(e);
+  });
+}
+
+function Article(article) {
+  // Create Elements
+  const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+      const imgContainer = document.createElement('div');
+        const image = document.createElement('img');
+      const name = document.createElement('span');
+
+  // Set content
+  headline.textContent = article.headline;
+    image.setAttribute('src',article.authorPhoto);
+    name.textContent = article.authorName;
+
+  // Create Structure
+  card.appendChild(headline);
+  card.appendChild(author);
+    author.appendChild(imgContainer);
+      imgContainer.appendChild(image);
+    author.appendChild(name);
+
+  // Apply styles
+  card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+      imgContainer.classList.add('img-container');
+
+  return card;
+}
--- components/Header/index.js
+++ components/Header/index.js
@@ -3,10 +3,38 @@
 // Using a function create the component you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  // Create Elements
+  const header = document.createElement('div');
+  const date = document.createElement('span');
+  const heading = document.createElement('h1');
+  const temperature = document.createElement('span');
+
+  // Set content
+  var today  = new Date();
+  var options = { month: 'long', day: 'numeric', year: 'numeric' };
+  date.textContent = today.toLocaleDateString("en-US", options).toUpperCase();
+  heading.textContent = "Lambda Times";
+  temperature.textContent = "98°";
+
+  // Create Structure
+  header.appendChild(date);
+  header.appendChild(heading);
+  header.appendChild(temperature);
+
+  // Apply styles
+  header.classList.add('header');
+  date.classList.add('date');
+  temperature.classList.add('temp');
+
+  return header;
+}
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header());
